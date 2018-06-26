51e48db5081254a85d2c887dd7b2f10477e2fe90
jmoypace <jmoypace@gmail.com>
2018-06-26 02:42:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@
 #  sentence.split.collect {|word| word.reverse}.join(" ")
 #end
 
-def reverse_each_word(sentence)
-newarr=sentence.split(" ")
-newarr1=[]
-newarr.collect do |word|
-newarr1.push(word.reverse)
-end
- newarr1.join(" ")
-end
+#def reverse_each_word(sentence)
+#newarr=sentence.split(" ")
+#newarr1=[]
+#newarr.collect do |word|
+#newarr1.push(word.reverse)
+#end
+# newarr1.join(" ")
+#end
